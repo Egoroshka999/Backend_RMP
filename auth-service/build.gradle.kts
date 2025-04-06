97ec73bea908eb8279cfc0955c6ce8cc2305390a
Егор Деревягин <45118129+Egoroshka999@users.noreply.github.com>
2025-04-06 21:48:24 +0300

    fix gradle

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.20"
     application
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
 }
 
 group = "com.Backend_RMP"
