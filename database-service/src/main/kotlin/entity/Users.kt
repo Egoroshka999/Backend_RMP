457299016647f8ce3189a38c9a2ca8a788f1490c
Nikita <nik3152815@gmail.com>
2025-05-08 16:30:47 +0300

    entities fix user delete log file

--- database-service/src/main/kotlin/entity/Users.kt
+++ database-service/src/main/kotlin/entity/Users.kt
@@ -6,8 +6,8 @@ import org.jetbrains.exposed.sql.Column
 object Users : IntIdTable() {
     val username = varchar("username", 50).uniqueIndex()
     val password = varchar("password", 100)
-    val weight: Column<Float> = float("weight")
-    val height: Column<Float> = float("height")
-    val age: Column<Int> = integer("age")
-    val goal: Column<String> = varchar("goal", 50)
+//    val weight: Column<Float> = float("weight")
+//    val height: Column<Float> = float("height")
+//    val age: Column<Int> = integer("age")
+//    val goal: Column<String> = varchar("goal", 50)
 }
